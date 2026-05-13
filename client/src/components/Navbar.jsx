89058368751eaf525c7f2b523fecd4d914e57f13
Javier <J_sedanomar@uncg.edu>
2026-05-13 18:57:42 -0400

    added styling to nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 export default function Navbar(){
     return (
-        <nav>
-            <div>
+        <nav className = "bg-gray-800 text-black p-4 flex items-center justify-center">
+            
                 <h1>Game Management System</h1>
-            </div>
+            
         </nav>
     )
 }
