14731f6b106fb156424bc500f00934597439821b
Javier <J_sedanomar@uncg.edu>
2026-05-13 18:00:01 -0400

    add user and game to beginning of routes

--- server/routes/web.php
+++ server/routes/web.php
@@ -4,19 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\GameController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 //crud routes for both games and users
 //search routes for games
-Route::post('/register', [UserController::class, 'createUser']);
-Route::post('/logout', [UserController::class, 'logout']);
-Route::post('/login', [UserController::class, 'login']);
+Route::get('/user', [UserController::class, 'getUser']);
+Route::post('/user/register', [UserController::class, 'createUser']);
+Route::post('/user/logout', [UserController::class, 'logout']);
+Route::post('/user/login', [UserController::class, 'login']);
+
 
 //protected routes for games
 Route::middleware('auth')->group(function () {
     Route::post('/game', [GameController::class, 'createGame']);
-    Route::get('/games', [GameController::class, 'getGames']);
+    Route::get('/game', [GameController::class, 'getGames']);
     Route::put('/game/{game_id}', [GameController::class, 'updateGame']);
     Route::get('/game/{game_id}', [GameController::class, 'getGame']);
     Route::delete('/game/{game_id}', [GameController::class, 'deleteGame']);
