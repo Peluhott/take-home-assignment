1ca8b6ce55e302175d38d08a7a61463127cf1e83
Javier <J_sedanomar@uncg.edu>
2026-05-13 17:57:10 -0400

    sidebar

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+export default function Sidebar(){
+    return (
+        <div>
+            <h1>Sidebar</h1>
+            <ul>
+                <li>this</li>
+                <li>is</li>
+                <li>where</li>
+                <li>links</li>
+                <li>will</li>
+                <li>go</li>
+
+            </ul>
+        </div>
+    )
+}
