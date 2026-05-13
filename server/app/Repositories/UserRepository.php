ac051c10f8815ed457b6fd67ae30ad9edd35898c
Javier <J_sedanomar@uncg.edu>
2026-05-13 11:24:35 -0400

    added UserRepository with methods for creating, retrieving, updating, and deleting users

---
+++ server/app/Repositories/UserRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserRepository
+{
+    public function createUser(string $name, string $email, string $password)
+    {
+        return DB::insert('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [$name, $email, $password]);
+    }
+
+    public function getUser(int $user_id)
+    {
+        return DB::select('SELECT * FROM users where id = ?', [$user_id]);
+    }
+
+    public function updateUser(int $user_id, string $name, string $email, string $password)
+    {
+        return DB::update('UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?', [$name, $email, $password, $user_id]);
+    }
+
+    public function deleteUser(int $user_id)
+    {
+        return DB::delete('DELETE FROM users WHERE id = ?', [$user_id]);
+    }
+}
