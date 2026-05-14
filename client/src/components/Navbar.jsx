5215c16b57195dbef07f893d1e863b302b60aecc
Javier <J_sedanomar@uncg.edu>
2026-05-13 21:49:32 -0400

    made navbar take full width

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar(){
     return (
-        <nav className = "bg-gray-800 text-black p-4 flex items-center justify-center">
+        <nav className = " w-full bg-gray-800 text-black p-4 flex items-center justify-center">
             
                 <h1>Game Management System</h1>
             
