5168ece703aef540fc2d9e5775506a1c8c5e5239
Javier <J_sedanomar@uncg.edu>
2026-05-14 08:20:35 -0400

    made the game card clickable to show update form, added styling

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,10 +1,26 @@
 
-
-export default function GameCard({game}){
+export default function GameCard({game, onClick}){
     return (
-        <div>
-            <img src={game.image_url} alt={game.title}/>
-            <h2>{game.title}</h2>
-        </div>
+      <div
+        className="bg-white rounded-xl shadow-md overflow-hidden w-72 hover:shadow-xl transition-shadow duration-300 cursor-pointer"
+        onClick={onClick}
+      >
+
+    <img
+        src={game.image_url || "/defaultgame.avif"}
+        alt={game.title}
+        className="w-full h-[18.5rem] object-cover"
+    />
+
+    <div className="p-4">
+        <h2 className="text-xl font-bold text-gray-800">
+            {game.title}
+        </h2>
+
+        <p className="text-gray-600 mt-2">
+            Rating: {game.rating ?? "No rating"}
+        </p>
+    </div>
+</div>
     )
 }
