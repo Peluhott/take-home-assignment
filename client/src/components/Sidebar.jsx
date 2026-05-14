204adc2bb108a2d1c488748d1c17b5e9f6202eb2
Javier <J_sedanomar@uncg.edu>
2026-05-14 08:23:02 -0400

    added callback function so sidebar sets the content in main content div,  added logout function, added styling

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,15 +1,38 @@
-export default function Sidebar(){
+import axios from "axios";
+
+export default function Sidebar({setActiveView}){
+    async function handleLogout() {
+        try {
+            await axios.post("/user/logout");
+            window.location.reload();
+        } catch (error) {
+            console.error("Error logging out:", error.response?.data || error);
+        }
+    }
+
     return (
         <div className= "w-64 min-h-screen bg-gray-200 text-black space-y-4 p-4">
-            <h1>Sidebar</h1>
             <ul className = "space-y-4">
-                <li>View Games</li>
-                <li>Create Game</li>
-                
-                <li>View Platforms</li>
-                <li>Add Platform</li>
-                <li>Profile</li>
-                <li>Logout</li>
+                <li
+                    onClick={() => setActiveView("viewGames")}
+                    className = "cursor-pointer hover:text-blue-500"
+                >View Games</li>
+                <li
+                    onClick={() => setActiveView("createGame")}
+                    className = "cursor-pointer hover:text-blue-500"
+                >Create Game</li>
+                <li
+                    onClick={() => setActiveView("viewPlatforms")}
+                    className = "cursor-pointer hover:text-blue-500"
+                >View Platforms</li>
+                <li
+                    onClick={() => setActiveView("viewProfile")}
+                    className = "cursor-pointer hover:text-blue-500"
+                >Profile</li>
+                <li
+                    onClick={handleLogout}
+                className ="cursor-pointer hover:text-blue-500"
+                >Logout</li>
                 
             </ul>
         </div>
