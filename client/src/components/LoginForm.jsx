1e53fee60536aaecf12c91c969b550ca465ea0ba
Javier <J_sedanomar@uncg.edu>
2026-05-14 08:20:47 -0400

    added styling

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -47,6 +47,7 @@ export default function LoginForm({onSwitchToRegister}) {
             </button>
             <button
                 type="button"
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
                 onClick={onSwitchToRegister}>
                 Register
 
